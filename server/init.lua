ca7a8880a5451d18a52f5d861cb647f9804f858a
Primordial-Devv <primordial.devv@gmail.com>
2026-06-20 23:48:58 +0200

    FIX - Fixing migration rce condition

--- server/init.lua
+++ server/init.lua
@@ -1,4 +1,8 @@
 MySQL.ready(function()
+    while not GlobalState['gaia_core:state:schemaReady'] do
+        Wait(50)
+    end
+
     Gaia.migration.run(MigrationConfig)
     Gaia.print.success('Multicharacter initialization complete')
 end)
